d430c0767714db435124d4a46c435c0b9a71c13c
yoki <petras.moil@gmail.com>
2020-03-09 23:33:36 +0200

    new push1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let app = express();
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
   console.log("Got a GET request for the homepage");
-  res.send('Hello GET GET...');
+  res.send('Hello GET GET...xcx');
 });
 
 // This responds a POST request for the homepage
