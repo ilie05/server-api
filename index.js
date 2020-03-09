432f1832a4e0896c1c59facdf2dac3cdea0bd0c4
yoki <petras.moil@gmail.com>
2020-03-09 23:09:34 +0200

    be server

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = process.env.PORT || 5000;
+
+var express = require('express');
+var app = express();
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+  console.log("Got a GET request for the homepage");
+  res.send('Hello GET');
+});
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+  console.log("Got a POST request for the homepage");
+  res.send('Hello POST');
+});
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+  console.log("Got a DELETE request for /del_user");
+  res.send('Hello DELETE');
+});
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+  console.log("Got a GET request for /list_user");
+  res.send('Page Listing');
+});
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {
+  console.log("Got a GET request for /ab*cd");
+  res.send('Page Pattern Match');
+});
+
+var server = app.listen(PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port)
+});
